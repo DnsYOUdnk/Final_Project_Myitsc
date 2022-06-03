898ff818dcd5670bd6e05d371f12b1fe4f25257d
Dinis <d.yudenka@yandex.by>
2022-06-04 02:07:28 +0300

    fix components searchInput

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -3,12 +3,12 @@ import { dataContext } from '../../dataContext/dataContext'
 import './search_input.css'
 
 export const SearchInput = () =>{
-    const {setSearchValue} = useContext(dataContext)
+    const {searchValue, setSearchValue} = useContext(dataContext)
     const handleChange = (e) => {
-        setSearchValue(e.target.value.toLowerCase())
+        setSearchValue(e.target.value.toLowerCase());
     }
     
     return (
-        <input type='text' className='page__movie__input' onChange={(e) => handleChange(e)} id='page__movie__input__value' placeholder='Search'/>
+        <input type='text' className='page__movie__input' value={searchValue} onChange={(e) => handleChange(e)} id='page__movie__input__value' placeholder='Search'/>
     )
 }
